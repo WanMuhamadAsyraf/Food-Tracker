9b17eb9096e0e49676bee5a4a61f8aa6ef142f58
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2015-03-09 16:38:48 -0700

    swapped migrations to placate postgres (hopefully)

--- db/migrate/20141021124444_create_food_list.rb
+++ db/migrate/20141021124444_create_food_list.rb
@@ -1,10 +1,20 @@
 class CreateFoodList < ActiveRecord::Migration
-  def change
-    create_table :foods do |t|
-      t.references :list
-      t.string :food_name
-      t.integer :calories
+ def change
+    create_table :lists do |t|
+      t.string :list_name
       t.timestamps
     end
   end
 end
+
+
+# class CreateFoodList < ActiveRecord::Migration
+#   def change
+#     create_table :foods do |t|
+#       t.references :list
+#       t.string :food_name
+#       t.integer :calories
+#       t.timestamps
+#     end
+#   end
+# end
--- db/migrate/20141021124520_create_food_table.rb
+++ db/migrate/20141021124520_create_food_table.rb
@@ -1,8 +1,20 @@
 class CreateFoodTable < ActiveRecord::Migration
   def change
-    create_table :lists do |t|
-      t.string :list_name
+    create_table :foods do |t|
+      t.references :list
+      t.string :food_name
+      t.integer :calories
       t.timestamps
     end
   end
 end
+
+
+# class CreateFoodTable < ActiveRecord::Migration
+#   def change
+#     create_table :lists do |t|
+#       t.string :list_name
+#       t.timestamps
+#     end
+#   end
+# end
